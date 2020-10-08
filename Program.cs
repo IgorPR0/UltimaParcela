a7295ddbaa71388aef9e75ae93192451be2e7bcb
IgorPR0 <igortecnologia2020@gmail.com>
2020-10-07 22:00:42 -0300

    Concluido atividade UltimaParcela

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorInt , Parcelas , Conta;
+
+            DateTime Data = DateTime.Now;
+            DateTime ValorPar;
+
+            Console.Write("Valor inteiro= ");
+            valorInt = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Quantidade de Parcelas= ");
+            Parcelas = Convert.ToDouble(Console.ReadLine());
+
+            Conta = valorInt / Parcelas;
+
+            ValorPar = Data.AddMonths(8);
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"\nSucesso ultima parcela disponivel em: {ValorPar}, no valor de R${Conta}");
+            Console.ResetColor();
+        }
+    }
+}
